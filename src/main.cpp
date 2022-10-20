eee5c7c0e71c175673becba3b691a3d919ba16b6
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-10-21 02:05:01 +0400

    feat: init ground

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 #include "window.h"
 #include "resources.h"
 
+#define WINDOW_X 1280
+#define WINDOW_Y 720
+
 void init_libraries() {
     if (SDL_Init(SDL_INIT_VIDEO) > 0)
         std::cout << "HEY.. SDL_Init HAS FAILED. SDL_ERROR: " << SDL_GetError() << std::endl;
@@ -16,18 +19,28 @@ void init_libraries() {
         std::cout << "IMG_init has failed. Error: " << SDL_GetError() << std::endl;
 }
 
+void init_ground(std::vector<std::shared_ptr<Entity>> *list, Resources* resources) {
+    SDL_Texture* grassTexture = resources->getGrassTexture();
+    int w, h;
+    SDL_QueryTexture(grassTexture, NULL, NULL, &w, &h);
+    
+    int count = WINDOW_X / h;
+    int low = WINDOW_Y - (2 * w);
+    std::cout << "w: " << w  << " h: " << h << " count: " << count << " low: " << low << std::endl;
+    
+    for (int i = 0; i < count/3; i++) {
+        list->push_back(std::make_shared<Entity>(Vector2f(i * h, low/4), resources->getGrassTexture()));
+    }
+}
+
 int main(int argc, char *args[]) {
-    Window window("GAME v1.0", 1280, 720);
+    Window window("GAME v1.0", WINDOW_X, WINDOW_Y);
     
     //todo: make singleton
     Resources* resources = new Resources(&window);
 
-    std::vector<std::shared_ptr<Entity>> entities = {
-        std::make_shared<Entity>(Vector2f(0, 0), resources->getGrassTexture()),
-        std::make_shared<Entity>(Vector2f(30, 0), resources->getGrassTexture()),
-        std::make_shared<Entity>(Vector2f(30, 30), resources->getGrassTexture()),
-        std::make_shared<Entity>(Vector2f(30, 60), resources->getGrassTexture()),
-    };
+    std::vector<std::shared_ptr<Entity>> entities = {};
+    init_ground(&entities, resources);
 
     bool gameRunning = true;
 

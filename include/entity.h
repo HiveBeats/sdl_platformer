ef47a897e677ceef235653ad29878271d686f834
HiveBeats <38073817+HiveBeats@users.noreply.github.com>
2022-10-20 22:29:05 +0400

    feat: basic entity api

--- include/entity.h
+++ include/entity.h
@@ -4,19 +4,28 @@
 
 #include <vector2.h>
 
-class Entity
-{
-public:
-	Entity(Vector2f p_pos, SDL_Texture* p_tex);
-	Vector2f& getPos()
-	{
-		return pos;
-	}
-	SDL_Texture* getTex();
-	SDL_Rect getCurrentFrame();
+class Entity {
 private:
-	Vector2f pos;
+    Vector2f position;
+    SDL_Rect currentFrame;
+    SDL_Texture *texture;
 
-	SDL_Rect currentFrame;
-	SDL_Texture* tex;
+public:
+    Entity(Vector2f p_position, SDL_Texture *p_texture);
+
+    Vector2f getPosition();
+    float getX();
+    float getY();
+	void setPosition(Vector2f p_position);
+    void setX(float p_x);
+    void setY(float p_y);
+
+
+    int getWidth();
+    int getHeight();
+
+    SDL_Texture *getTexture();
+    void setTexture(SDL_Texture *p_texture);
+
+    SDL_Rect getCurrentFrame();
 };
--- src/entity.cpp
+++ src/entity.cpp
@@ -2,21 +2,49 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-Entity::Entity(Vector2f p_pos, SDL_Texture* p_tex)
-:pos(p_pos), tex(p_tex)
-{
-	currentFrame.x = 0;
-	currentFrame.y = 0;
-	currentFrame.w = 32;
-	currentFrame.h = 32;
+Entity::Entity(Vector2f p_position, SDL_Texture *p_texture) : position(p_position), texture(p_texture) {
+    currentFrame.x = 0;
+    currentFrame.y = 0;
+    // set frame width and height by it's texture size;
+    SDL_QueryTexture(p_texture, NULL, NULL, &currentFrame.w, &currentFrame.h);
 }
 
-SDL_Texture* Entity::getTex()
-{
-	return tex;
+Vector2f Entity::getPosition() {
+    return position;
 }
 
-SDL_Rect Entity::getCurrentFrame()
-{
-	return currentFrame;
+float Entity::getX() {
+    return position.x;
+}
+
+float Entity::getY() {
+    return position.y;
+}
+
+void Entity::setPosition(Vector2f p_position) {
+    position = p_position;
+}
+
+void Entity::setX(float p_x) {
+    position.x = p_x;
+}
+
+void Entity::setY(float p_y) {
+    position.y = p_y;
+}
+
+int Entity::getWidth() {
+    return currentFrame.w;
+}
+
+int Entity::getHeight() {
+    return currentFrame.h;
+}
+
+SDL_Texture *Entity::getTexture() {
+    return texture;
+}
+
+SDL_Rect Entity::getCurrentFrame() {
+    return currentFrame;
 }
